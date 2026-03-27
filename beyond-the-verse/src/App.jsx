192eb391f1dd7300a928cb6531b0dd5aa7a2e420
rohantiwari2003 <rohantiwari0442@gmail.com>
2026-03-27 13:18:56 +0530

    Update App.jsx

--- beyond-the-verse/src/App.jsx
+++ beyond-the-verse/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-// Dhyan dein: Agar folder ka naam alag hai, toh path theek kar lena
-import HomePage from './pages/Home/HomePage'; 
+import HomePage from './components/HomePage/HomePage';
+import DonationPage from './components/DonationPage/DonationPage';
 
 export default function App() {
   return (
