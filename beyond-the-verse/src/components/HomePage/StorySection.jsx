3e2fc8041a1a0c295ddcdfff0921c3446fd78c20
rohantiwari2003 <rohantiwari0442@gmail.com>
2026-03-27 13:49:50 +0530

    Update StorySection.jsx

--- beyond-the-verse/src/components/HomePage/StorySection.jsx
+++ beyond-the-verse/src/components/HomePage/StorySection.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { collection, onSnapshot, query, orderBy } from "firebase/firestore";
-import { db } from "../firebase";
+import { db } from "../components/firebase";
 
 export default function StorySection() {
   const [subjects, setSubjects] = useState([]);
