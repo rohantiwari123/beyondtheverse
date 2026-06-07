79fac9dc9fb4987a65357b18b0a87ea805eed1b4
rohan <126929427+rohantiwari123@users.noreply.github.com>
2026-06-07 00:39:28 +0000

    chore: remove Vercel and move backend to standalone folder

--- beyond-the-verse/vite.config.js
+++ beyond-the-verse/vite.config.js
@@ -5,7 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // 🌟 Vercel Deployment (Root)
+  base: './', // 🌟 Relative paths (Works everywhere)
   
   plugins: [
     react(),
